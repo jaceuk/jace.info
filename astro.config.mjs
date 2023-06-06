c65103597c57ef57b2770861efcc01ed4c5d60c8
jaceuk <jnewington@gmail.com>
2023-06-06 20:14:58 +0100

    Trying to fix 404 problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
-	build: {
-		// Example: Generate `page.html` instead of `page/index.html` during build.
-		format: 'file'
-	}
+  integrations: [react()],
+  build: {
+    // Example: Generate `page.html` instead of `page/index.html` during build.
+    format: 'file'
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
