bd78bb8aa011584e7864235a6c3e2a1748137379
jaceuk <jnewington@gmail.com>
2023-06-06 19:42:55 +0100

    404 not working on live

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,11 @@ export default defineConfig({
 	build: {
 		// Example: Generate `page.html` instead of `page/index.html` during build.
 		format: 'file'
-	}
+	},
+	rewrites: [
+		{
+			source: '(.*)',
+			destination: '404.html'
+		}
+	]
 });
